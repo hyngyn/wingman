20358db02e19de30c7acf0b787392ca4a2f4af3b
hyngyn <hyngyn@gmail.com>
2013-05-24 11:20:15 -0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wingman::Application.config.secret_token = '377ae0e45706bc30982e0ac33240ea351e91cc1d8e4d956a269dffae7efa9967c08d1ca8885553f7efe565f44a022ca7e0637a79f652bf83590e58283682f172'
