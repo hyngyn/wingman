c6f7f14d018baf306f8ed9322091676fffcd9857
hyngyn <hyngyn@gmail.com>
2013-06-01 13:28:09 -0700

    Added comments to controller, major styling changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,6 @@
+// home.js
+// handles toggling showing and hiding error messages/value prop during search
+
 $(function() {
  $("form").on("ajax:success", function(xhr, data, status) {
     $('.no_results').addClass("hidden");
@@ -6,6 +9,7 @@ $(function() {
 
   $("form").on("ajax:error", function(xhr, data, status) {
     $('.no_results').removeClass("hidden");
+    $('#value_prop').addClass("hidden");
   });
 
   $("form").on("ajax:before", function(xhr, data, status) {
@@ -18,10 +22,12 @@ $(function() {
 
   $("#new_search").on("click", function(){
     $('.no_results').addClass("hidden");
+    $('#value_prop').removeClass("hidden");
   });
 
   $(".btn-primary").on("click", function(){
     $('.no_results').addClass("hidden");
+    $('#value_prop').removeClass("hidden");
   });
 
   $("#start_date").datepicker();
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,19 @@ class PagesController < ApplicationController
 
   EVENTBRITE_CATEGORIES = "entertainment, others, performances, social, sports, travel, fairs, food, music, recreation"
 
+  # landing page
   def home
-
   end
   
-  #search driver
+  # search driver
+  #
+  # inputs: params = {  location = user select string location as "San Francisco, CA"
+  #                     interests_list = user input string with comma separated interests
+  #                     start_date = user select start date with format
+  #                     end_date =  user select start date with format  } 
+  #
+  # outputs: successs = partial 'search_results' if there are results
+  #         fail = renders empty json and status 500 if no results    
   def search
     location = params[:location].to_s
     interests_list = params[:interests].to_s
@@ -23,17 +31,18 @@ class PagesController < ApplicationController
     city, region = get_city_region_from_input(location)
     interests = interests_list.split(',').join('%20OR%20').gsub(' ', '')
    
-    eventbrite_date_array = generate_date_array(start_date, end_date)
+    date_array = generate_date_array(start_date, end_date)
     forecast_days = get_weather(city, region)
 
-    filtered_dates = filter_bad_weather(forecast_days, eventbrite_date_array)
+    filtered_dates = filter_bad_weather(forecast_days, date_array)
 
-    results = eventbrite_api_search(interests, city, region, eventbrite_date_array) rescue nil
+    results = eventbrite_api_search(interests, city, region, date_array) rescue nil
     return render :json => {}, :status => 500 if results.blank?
     summary = results.first.last.shift
     events = results.first.last
-    @stripped_events = strip_event_results(events, filtered_dates)
-    @stripped_events = @stripped_events.first(5)
+
+    #strip events of days with bad forecast and take first 5
+    @stripped_events = strip_event_results(events, filtered_dates).first(5)
 
     return render :json => {}, :status => 500 if @stripped_events.blank?
 
@@ -42,27 +51,43 @@ class PagesController < ApplicationController
 
   private
 
-    def filter_bad_weather(forecast_days, eventbrite_date_array)
-      eventbrite_date_array.reject!{|day| BAD_WEATHER.include?(forecast_days[day]) }
-      return eventbrite_date_array.blank? ? [] : eventbrite_date_array
+    # filter_bad_weather = filters events with bad forecasts
+    #
+    # inputs: forecast_days = hash of dates and corresponding forecast
+    #         date_array = user selected dates in an array
+    #
+    # outputs: date_array = an array of days that have good forecast
+    def filter_bad_weather(forecast_days, date_array)
+      date_array.reject!{|day| BAD_WEATHER.include?(forecast_days[day]) }
+      return date_array.blank? ? [] : date_array
     end
 
+    # get_weather = get full weather report for each day from weatherunderground
+    #
+    # input: city = string of city i.e. "San Francisco"
+    #        region = string of region i.e. "CA"
+    #
+    # outputs: forecast_days = hash of days (string) as keys and day forecast (string) as values i.e. {"2013/06/01": "rain"}
     def get_weather(city, region)
       w_api = Wunderground.new("2064437974116822")
-      forecast = w_api.forecast10day_for(region,city.gsub(" ","%20"))["forecast"]["txt_forecast"]["forecastday"]
+      forecast = w_api.forecast10day_for( region, city.gsub(" ","%20") )["forecast"]["txt_forecast"]["forecastday"]
       forecast_days =  {}
 
+      # API call returns day and night forecast, we only want the day forecast
       forecast.each_with_index do |item,index| 
         if index % 2 == 0 
           day = (Time.now + (index/2).days).strftime("%Y/%m/%d").to_s
- 
           forecast_days[day] = item["icon"]
         end
       end
       forecast_days
     end
 
-    #strip full events hash to relevant info to display in view
+    # strip_events_results = strip full events of bad weather days
+    #
+    # input: events = array of events
+    #        good_days = events of good forecast days
+    # outputs: stripped_events= array of events without bad forecast days
     def strip_event_results(events, good_days)
       stripped_events = []
       events.each do |e|
@@ -86,7 +111,11 @@ class PagesController < ApplicationController
       stripped_events.reject{|event| good_days.include?(event[:date].to_s)}
     end
 
-    #split location string and return city, region
+    # get_city_region_from_input: split location string and return city, region
+    #
+    # inputs: location = string of city and region as "city, region"
+    #
+    # outputs: city, region as "City", "REGION"
     def get_city_region_from_input(location)
       city_region = location.split(',')
       city = city_region.first.strip.titleize rescue ""
@@ -95,7 +124,14 @@ class PagesController < ApplicationController
     end
 
 
-    #eventbrite api call
+    # eventbrite_api_search = eventbrite api call
+    #
+    # inputs: interests = string of formatted interests
+    #         city = string of city
+    #         region = string of region
+    #         date array = array of dates (strings)
+    #
+    # output: response = raw eventbrite response of events in JSON
     def eventbrite_api_search(interests, city, region, date_array)
       eb_auth_tokens = { app_key: 'HKZFAX6AT4QX2JVNN7',
                          user_key: '134983204943172706728' }
@@ -111,13 +147,25 @@ class PagesController < ApplicationController
                                           category: EVENTBRITE_CATEGORIES })
     end
 
-    #generate date string for eventbrite api call
+    # generate_date_string = generates date string for eventbrite api call
+    #
+    # inputs: start_date = string in date format yyyy/mm/dd
+    #         end_date = string in date format yyyy/mm/dd
+    #
+    # outputs: eventbrite_date = a string with the start date to end date in format "yyyy-mm-dd yyyy-mm-dd" 
     def generate_date_string(start_date, end_date)
       start_array = start_date.scan(/[0-9]+/)
       end_array = end_date.scan(/[0-9]+/)
+
       eventbrite_date =  start_array.join('-') + " " + end_array.join('-')
     end
 
+    # generate_date_array = get arrays of days in date format yyyy/mm/dd
+    # 
+    # inputs: start_date = user input start date
+    #         end_date = user input end date
+    #
+    # outputs: date_array = an array of dates (strings) in date format yyyy/mm/dd
     def generate_date_array(start_date, end_date)
       start_array = start_date.scan(/[0-9]+/)
       end_array = end_date.scan(/[0-9]+/)
